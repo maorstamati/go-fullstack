f3baa9a416ab92045af499ff476643c353879517
mstamati <maorstamati@gmail.com>
2019-04-11 02:17:43 +0300

    routing controllers and authentication

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Thing = require('./models/thing');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://Maor:dGS9NtcdO8H6IFVV@cluster0-rdaxc.mongodb.net/test?retryWrites=true').
-    then(() => {
-        console.log('Successfully connected to MongoDB Atlas!');
-    }).
-    catch((error) => {
-        console.log('Unable to connect to MongoDB Atlas!');
-        console.error(error);
-    });
+then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+}).
+catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+});
 
 /*
  CORS stands for Cross Origin Resource Sharing. It is a standard that allows us to relax default security rules
@@ -33,50 +34,8 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-    const thing = new Thing({
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId
-    });
-    console.log(thing);
-    thing.save().then(
-        () => {
-            res.status(201).json({
-                message: 'Post saved successfully!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-});
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
-app.use('/api/stuff', (req,res,next) => {
-    const stuff = [
-        {
-            _id: 'dsjkfhasjklf',
-            title: 'My first thing',
-            description: 'My first great thing',
-            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/48/%C5%A0eduvos_RKB_1844-1850_prie%C5%A1santuokin%C4%97s_apklausos_knyga_097.jpg',
-            price: 4900,
-            userId: 'sdfjkghq3784ty'
-        },
-        {
-            _id: 'dsjkfhasjk11',
-            title: 'My second thing',
-            description: 'My second great thing',
-            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/02/Starr-050208-3926-Prosopis_pallida-habitat-North_coast-Kahoolawe_%2824369072199%29.jpg',
-            price: 5100,
-            userId: 'sdfjkghq3784ty'
-        }
-    ];
-    res.status(200).json(stuff);
-});
 
 module.exports = app;
