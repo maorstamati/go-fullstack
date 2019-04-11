9f9062835fc1b6267a98dd58ebefa856660f522e
mstamati <maorstamati@gmail.com>
2019-04-11 04:37:03 +0300

    Completed course

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,12 +1,15 @@
 const Thing = require('../models/thing');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
-  const thing = new Thing({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
+    const url = req.protocol + '://' + req.get('host');
+    req.body.thing = JSON.parse(req.body.thing);
+    const thing = new Thing({
+    title: req.body.thing.title,
+    description: req.body.thing.description,
+    imageUrl: url + '/images/' + req.file.filename,
+    price: req.body.thing.price,
+    userId: req.body.thing.userId
   });
   thing.save().then(
     () => {
@@ -40,43 +43,64 @@ exports.getOneThing = (req, res, next) => {
 };
 
 exports.modifyThing = (req, res, next) => {
-  const thing = new Thing({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  Thing.updateOne({_id: req.params.id}, thing).then(
-    () => {
-      res.status(201).json({
-        message: 'Thing updated successfully!'
-      });
+    let thing = new Thing({ _id: req.params._id });
+    if (req.file) {
+        const url = req.protocol + '://' + req.get('host');
+        req.body.thing = JSON.parse(req.body.thing);
+        thing = {
+            _id: req.params.id,
+            title: req.body.thing.title,
+            description: req.body.thing.description,
+            imageUrl: url + '/images/' + req.file.filename,
+            price: req.body.thing.price,
+            userId: req.body.thing.userId
+        };
+    } else {
+        thing = {
+            _id: req.params.id,
+            title: req.body.title,
+            description: req.body.description,
+            imageUrl: req.body.imageUrl,
+            price: req.body.price,
+            userId: req.body.userId
+        };
     }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
+    Thing.updateOne({_id: req.params.id}, thing).then(
+        () => {
+            res.status(201).json({
+                message: 'Thing updated successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
 };
 
 exports.deleteThing = (req, res, next) => {
-  Thing.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
+    Thing.findOne({_id: req.params.id}).then(
+        (thing) => {
+            const filename = thing.imageUrl.split('/images/')[1];
+            fs.unlink('images/' + filename, () => {
+                Thing.deleteOne({_id: req.params.id}).then(
+                    () => {
+                        res.status(200).json({
+                            message: 'Deleted!'
+                        });
+                    }
+                ).catch(
+                    (error) => {
+                        res.status(400).json({
+                            error: error
+                        });
+                    }
+                );
+            });
+        }
+    );
 };
 
 exports.getAllThings = (req, res, next) => {
